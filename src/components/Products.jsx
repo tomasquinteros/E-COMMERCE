f8a4b21e83db1ae942627e575842e24863d0fe2f
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-23 17:13:11 -0300

    create product component & filters product

---
+++ src/components/Products.jsx
@@ -0,0 +1,28 @@
+import { AddFavorites, AddToCartIcon } from './Icons'
+
+export const Products = ({ products, addCart }) => {
+  return (
+    <section className='max-w-4xl'>
+      <ul className='grid grid-cols-1 gap-3'>
+        {
+          products.map(product =>
+            (
+              <li className=' bg-slate-500 flex flex-col items-center justify-center p-3' key={product.id}>
+                <img className='rounded-md w-full aspect-video block object-cover bg-white' src={product.thumbnail} alt={product.id} />
+                <h1>{product.title} - {product.price}</h1>
+                <section className='p-4'>
+                  <button className='rounded-md border-2 border-gray-600 p-2'>
+                    <AddToCartIcon />
+                  </button>
+                  <button className='rounded-md border-2 border-gray-600 p-2'>
+                    <AddFavorites />
+                  </button>
+                </section>
+              </li>
+            )
+          )
+        }
+      </ul>
+    </section>
+  )
+}
