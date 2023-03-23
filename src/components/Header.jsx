feec7149cbb5f793d57339f2ab605c3fcbb19a78
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-23 17:12:36 -0300

    create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <header className='bg-slate-900 w-full flex justify-between p-4'>
+      <h1 className='text-6xl'> SHOPPING CART </h1>
+      <button className='text-2xl'>Cart</button>
+    </header>
+  )
+}
