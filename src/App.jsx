78bc9962d0841c870a3ea522086f6426f0fb7064
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-23 17:12:14 -0300

    import products, footer, header, filters, mocks

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { Products } from './components/Products'
+import { Header } from './components/Header'
+import { Filters } from './components/Filters'
+import { Footer } from './components/Footer'
+import { useState } from 'react'
+import { products as initialState } from './mocks/product.json'
+import { useFilters } from './hooks/useFilters'
 
 function App () {
+  const [products] = useState(initialState)
+  const { filterProduct, filters, setFilters } = useFilters()
+  const filteredProduct = filterProduct(products)
+
   return (
-    <div className='App'>
-      E-COMMERCE
+    <div className='flex flex-col gap-20 items-center justify-center'>
+      <Header />
+      <div className='grid grid-cols-2 w-full'>
+        <Filters setFilters={setFilters} filters={filters} />
+        <Products products={filteredProduct} />
+      </div>
+      <Footer />
     </div>
   )
 }
