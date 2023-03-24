c42af2ad6f818f8cd5383e9102dddae4c68b2137
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-24 14:29:53 -0300

    import useCart custom hook

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,8 @@
 import { AddFavorites, AddToCartIcon } from './Icons'
+import { useCart } from '../hooks/useCart'
 
-export const Products = ({ products, addCart }) => {
+export const Products = ({ products }) => {
+  const { addToCart } = useCart()
   return (
     <section className='max-w-4xl'>
       <ul className='grid grid-cols-1 gap-3'>
@@ -11,7 +13,10 @@ export const Products = ({ products, addCart }) => {
                 <img className='rounded-md w-full aspect-video block object-cover bg-white' src={product.thumbnail} alt={product.id} />
                 <h1>{product.title} - {product.price}</h1>
                 <section className='p-4'>
-                  <button className='rounded-md border-2 border-gray-600 p-2'>
+                  <button
+                    className='rounded-md border-2 border-gray-600 p-2'
+                    onClick={() => addToCart(product)}
+                  >
                     <AddToCartIcon />
                   </button>
                   <button className='rounded-md border-2 border-gray-600 p-2'>
