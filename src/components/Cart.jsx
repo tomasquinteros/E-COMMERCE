8fd9f30f76b2dc61f2ec4e8fdd9b5c77f9f718eb
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-26 23:11:48 -0300

    create cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,26 @@
-export function Cart ({ cart }) {
+import { useCart } from '../hooks/useCart'
+
+function CartItem (product) {
+  const { title, thumbnail, price, quantity } = product
   return (
-    <div>
-      Cart: {cart}
-    </div>
+    <li>
+      <img src={thumbnail} alt={title} />
+      <h1>{title}</h1>
+      <p>Quantity: {quantity}</p>
+      <span>${price}</span>
+    </li>
+  )
+}
+
+export function Cart () {
+  const { cart } = useCart()
+  return (
+    <aside>
+      <ul>
+        {
+          cart.map(product => (<CartItem key={product} product={product} />))
+        }
+      </ul>
+    </aside>
   )
 }
