58e6b9da3b7e5e01ff4a2cc5eafba50f8501d6dd
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-27 23:46:34 -0300

    delete content, import cartcontext & useContext

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,24 +1,7 @@
-import { useState } from 'react'
+import { CartContext } from '../context/cart'
+import { useContext } from 'react'
 export function useCart () {
-  const [cart, setCart] = useState([])
+  const cart = useContext(CartContext)
 
-  const addToCart = product => {
-    const productInCartIndex = cart.findIndex(item => item.id === product.id)
-
-    if (productInCartIndex >= 0) {
-      const newCart = structuredClone(cart)
-      newCart[productInCartIndex].quantity += 1
-      return setCart(newCart)
-    }
-
-    return setCart(prevState => ([
-      ...prevState, { ...product, quantity: 1 }
-    ]))
-  }
-  const removeFromCart = product => {
-    const newCart = cart.filter(item => item.is !== product.id)
-    return setCart(newCart)
-  }
-
-  return { cart, setCart, addToCart, removeFromCart }
+  return cart
 }
