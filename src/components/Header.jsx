b8a459adae368f47ce2c9e6d3a3f300982241bc1
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-28 13:46:51 -0300

    change name const/addstyling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 export const Header = () => {
   return (
-    <header className='bg-slate-900 w-full flex justify-between p-4'>
-      <h1 className='text-6xl'> SHOPPING CART </h1>
-      <button className='text-2xl'>Cart</button>
+    <header className='bg-slate-900 w-full'>
+      <div className='max-w-7xl flex justify-between p-4 items-center m-auto'>
+        <h1 className='text-6xl'> SHOPPING CART </h1>
+        <button className='text-2xl'>Cart</button>
+      </div>
     </header>
   )
 }
