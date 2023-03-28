3a4ae90ce18cd0d730dd3a7dff1a953d79d64dc1
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-28 13:47:51 -0300

    create context Favorites and provider

---
+++ src/context/favorites.jsx
@@ -0,0 +1,28 @@
+import { createContext, useState } from 'react'
+// creamos el context
+export const FavoritesContext = createContext()
+
+// creamos el provider
+
+export const FavoritesProvider = ({ children }) => {
+  const [favorites, setFavorites] = useState([])
+
+  const addToFavorites = product => {
+    const productInFavorites = favorites.findIndex(item => item.id === product.id)
+    if (productInFavorites) {
+      const newFav = prevState => ([...prevState, { ...product }])
+      return setFavorites(newFav)
+    }
+  }
+  const removeFromFavorites = product => {
+    const newFav = favorites.filter(item => item.id !== product.id)
+    return setFavorites(newFav)
+  }
+  return (
+    <FavoritesContext.Provider
+      value={{ favorites, addToFavorites, removeFromFavorites }}
+    >
+      {children}
+    </FavoritesContext.Provider>
+  )
+}
