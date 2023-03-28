c1886f50b0398c96a07a0d1fc31f389c326d4ecc
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-27 23:45:08 -0300

    import provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { CartProvider } from './context/cart'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <CartProvider>
     <App />
-  </React.StrictMode>
+  </CartProvider>
 )
