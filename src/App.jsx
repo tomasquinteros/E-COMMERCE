740e14f36dfb0ae2f1faf6f73785f68b6e959846
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-29 23:24:18 -0300

    reformat document and import home/browser route

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,24 @@
-import { useState } from 'react'
-import { useFilters } from './hooks/useFilters'
-import { products as initialState } from './mocks/product.json'
 import { FavoritesProvider } from './context/favorites'
-
-import { Products } from './components/Products'
+import { Home } from './components/Home'
 import { Header } from './components/Header'
-import { Filters } from './components/Filters'
 import { Cart } from './components/Cart'
 import { Favorites } from './components/Favorites'
 import { Footer } from './components/Footer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App () {
-  const [products] = useState(initialState)
-  const { filterProduct, filters, setFilters } = useFilters()
-  const filteredProduct = filterProduct(products)
-
   return (
     <FavoritesProvider>
-      <div className='flex flex-col gap-20 items-center justify-center'>
-        <Header />
-        <div className='grid grid-cols-2 w-full gap-10 max-w-7xl'>
-          <Filters setFilters={setFilters} filters={filters} />
-          <Products products={filteredProduct} />
-        </div>
-        <Cart />
-        <Favorites />
-        <Footer />
+      <div className='flex flex-col gap-20 items-center justify-center m-auto'>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/favorites' element={<Favorites />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
       </div>
     </FavoritesProvider>
   )
