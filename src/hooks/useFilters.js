8f60eea5e38046086576db33404439dfd987db6c
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-29 23:26:37 -0300

    add conditional maxPrice and useState

--- src/hooks/useFilters.js
+++ src/hooks/useFilters.js
@@ -2,13 +2,14 @@ import { useState } from 'react'
 export function useFilters () {
   const [filters, setFilters] = useState({
     category: 'all',
-    minPrice: 0
+    minPrice: 0,
+    maxPrice: 1000
   })
 
   const filterProduct = (products) => {
     return products.filter(product => {
       return (
-        product.price >= filters.minPrice &&
+        product.price >= filters.minPrice & product.price <= filters.maxPrice &&
           (
             filters.category === 'all' ||
             product.category === filters.category
