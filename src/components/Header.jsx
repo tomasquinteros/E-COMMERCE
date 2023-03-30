87799321880a2cfd42a5c21aefbf39c520e4fa4b
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-29 23:25:35 -0300

    change header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
+import { NavLink } from 'react-router-dom'
+import { CartIcon, AddFavoritesIcon } from './Icons'
 export const Header = () => {
   return (
     <header className='bg-slate-900 w-full'>
-      <div className='max-w-7xl flex justify-between p-4 items-center m-auto'>
-        <h1 className='text-6xl'> SHOPPING CART </h1>
-        <button className='text-2xl'>Cart</button>
-      </div>
+      <nav className='max-w-7xl flex justify-between p-4 items-center m-auto'>
+        <NavLink className='text-6xl' to='/'> SHOPPING CART </NavLink>
+        <section className='flex gap-20'>
+          <NavLink className='flex gap-4 items-center' to='/cart'>
+            <span className='text-2xl'>Cart</span>
+            <CartIcon />
+          </NavLink>
+          <NavLink className='flex gap-4 items-center' to='/favorites'>
+            <span className='text-2xl'>Favorites</span>
+            <AddFavoritesIcon />
+          </NavLink>
+        </section>
+
+      </nav>
     </header>
   )
 }
