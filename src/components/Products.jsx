0081888afcca7f0a7e8c7504d67e1635be1bd2a4
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-29 23:26:17 -0300

    add conditional ternary

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,32 +2,33 @@ import { checkProductInCart, checkProductInFavorites } from '../functions/checkP
 import { useFavorites } from '../hooks/useFavorites'
 import { useCart } from '../hooks/useCart'
 import { ProductList } from './ProductList'
-
+import { NotFoundProduct } from './NotFoundProduct'
 export const Products = ({ products }) => {
   const { addToCart, cart, removeFromCart } = useCart()
   const { favorites, removeFromFavorites, addToFavorites } = useFavorites()
-
   return (
     <section className='max-w-4xl'>
       <ul className='grid grid-cols-1 gap-3'>
         {
-          products.slice(0, 10).map(product => {
-            const itemInCart = checkProductInCart(product, cart)
-            const itemInFav = checkProductInFavorites(product, favorites)
-            return (
-              <ProductList
-                key={product.id}
-                product={product}
-                itemInCart={itemInCart}
-                itemInFav={itemInFav}
-                addToCart={addToCart}
-                addToFavorites={addToFavorites}
-                removeFromCart={removeFromCart}
-                removeFromFavorites={removeFromFavorites}
-              />
+          products.length
+            ? products.slice(0, 10).map(product => {
+              const itemInCart = checkProductInCart(product, cart)
+              const itemInFav = checkProductInFavorites(product, favorites)
+              return (
+                <ProductList
+                  key={product.id}
+                  product={product}
+                  itemInCart={itemInCart}
+                  itemInFav={itemInFav}
+                  addToCart={addToCart}
+                  addToFavorites={addToFavorites}
+                  removeFromCart={removeFromCart}
+                  removeFromFavorites={removeFromFavorites}
+                />
+              )
+            }
             )
-          }
-          )
+            : <NotFoundProduct />
         }
       </ul>
     </section>
