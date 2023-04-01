b9dcdd875e34c75b7bdfe520cb3f115499128b3e
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-31 23:33:57 -0300

    styling cartlist comp & add decrementCart

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -1,22 +1,26 @@
 import { RemoveFromCartIcon } from './Icons'
-export function CartList ({ products, addToCart, removeFromCart }) {
+export function CartList ({ products, addToCart, removeFromCart, decrementCart }) {
   return (
     products.map(product => (
-      <li key={product.id}>
-        <img src={product.thumbnail} alt={product.title} />
+      <li key={product.id} className='flex items-center gap-6 p-2 border-y-2 border-slate-500'>
+        <img className='w-32 aspect-video block object-cover' src={product.thumbnail} alt={product.title} />
         <div>
           <h2>{product.title}</h2>
-          <span>{product.quantity} - {product.price}</span>
+          <span>${product.price}</span>
         </div>
-        <div>
-          <button onClick={() => addToCart(product)}>
+        <div className='text-2xl flex gap-6 items-center'>
+          <button onClick={() => addToCart(product)} className='bg-green-600 p-4 text-white font-extrabold'>
             +
           </button>
-          <button onClick={() => removeFromCart(product)}>
-            <RemoveFromCartIcon />
+          <span>{product.quantity}</span>
+          <button onClick={() => decrementCart(product)} className='bg-red-600 p-4 text-white font-extrabold'>
+            -
           </button>
-          <span>Total: ${product.total}</span>
         </div>
+        <button onClick={() => removeFromCart(product)}>
+          <RemoveFromCartIcon />
+        </button>
+        <span>Total: ${product.total}</span>
       </li>
     )
     )
