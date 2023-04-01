5b01ee4804e0e97b8df4081635862e0ec8ef6c81
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-31 23:33:43 -0300

    styling cart comp & add decrementCart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,11 +2,11 @@ import { useCart } from '../hooks/useCart'
 import { ClearCartIcon } from './Icons'
 import { CartList } from './CartList'
 export function Cart () {
-  const { cart, addToCart, removeFromCart, clearCart } = useCart()
+  const { cart, addToCart, removeFromCart, clearCart, decrementCart } = useCart()
   return (
     <aside>
-      <ul>
-        <CartList products={cart} addToCart={addToCart} removeFromCart={removeFromCart} />
+      <ul className='flex flex-col gap-6'>
+        <CartList products={cart} addToCart={addToCart} removeFromCart={removeFromCart} decrementCart={decrementCart} />
       </ul>
       <button onClick={() => clearCart()}>
         <ClearCartIcon />
