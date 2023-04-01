8da96243cada9d23fa0a69ebdd784fd3dca68609
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-04-01 00:42:16 -0300

    change icon ClearCart

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -58,7 +58,7 @@ export function RemoveFromCartIcon () {
 
 export function ClearCartIcon () {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' strokeWidth='1' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'>
+    <svg xmlns='http://www.w3.org/2000/svg' className='icon icon-tabler icon-tabler-shopping-cart-off' width='24' height='24' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'>
       <path stroke='none' d='M0 0h24v24H0z' fill='none' />
       <path d='M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0' />
       <path d='M17 17a2 2 0 1 0 2 2' />
