9a9d2c05d61c9c7fb876a58126b2c74b7f73e5cd
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-04-01 00:41:32 -0300

    add totalPrice

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,15 +2,18 @@ import { useCart } from '../hooks/useCart'
 import { ClearCartIcon } from './Icons'
 import { CartList } from './CartList'
 export function Cart () {
-  const { cart, addToCart, removeFromCart, clearCart, decrementCart } = useCart()
+  const { cart, addToCart, removeFromCart, clearCart, decrementCart, totalPrice } = useCart()
   return (
     <aside>
-      <ul className='flex flex-col gap-6'>
+      <ul className='flex flex-col gap-2'>
         <CartList products={cart} addToCart={addToCart} removeFromCart={removeFromCart} decrementCart={decrementCart} />
       </ul>
-      <button onClick={() => clearCart()}>
-        <ClearCartIcon />
-      </button>
+      <div className='flex px-8 py-2 items-center text-white gap-20'>
+        <button onClick={() => clearCart()} className='bg-red-600 p-4 gap-4 rounded-lg flex text-xl font-bold'>
+          <span>Clear Cart</span> <ClearCartIcon />
+        </button>
+        <span className='ml-auto bg-orange-600 p-4 rounded-md'>Total: ${totalPrice}</span>
+      </div>
     </aside>
   )
 }
