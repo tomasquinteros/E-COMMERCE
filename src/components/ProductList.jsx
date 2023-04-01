9f2b4cd30dd982deca05131f6505c242273be9da
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-31 23:35:29 -0300

    import buttonsIcon component and styling

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,28 +1,35 @@
-import { AddFavoritesIcon, AddToCartIcon, RemoveFromCartIcon, RemoveFromFavoritesIcon } from './Icons'
-
-export const ProductList = ({ product, removeFromCart, removeFromFavorites, itemInCart, itemInFav, addToCart, addToFavorites }) => {
+import { ExternalLink } from './Icons'
+import { ButtonAddOrRemoveCart, ButtonAddOrRemoveFavorites } from './Buttons'
+import { useCart } from '../hooks/useCart'
+import { useFavorites } from '../hooks/useFavorites'
+export const ProductList = ({ product, itemInCart, itemInFav }) => {
   const { title, id, price, thumbnail } = product
+  const { addToCart, removeFromCart } = useCart()
+  const { removeFromFavorites, addToFavorites } = useFavorites()
   return (
-    <li className=' bg-slate-500 flex flex-col items-center justify-center p-3' key={id}>
-      <img className='rounded-md w-full aspect-video block object-cover bg-white' src={thumbnail} alt={id} />
-      <h1>{title} - {price}</h1>
-      <section className='p-4'>
-        <button
-          className='rounded-md border-2 border-gray-600 p-2'
-          onClick={() => { itemInCart ? removeFromCart(product) : addToCart(product) }}
-        >
-          {
-            itemInCart
-              ? <RemoveFromCartIcon />
-              : <AddToCartIcon />
-          }
-        </button>
-        <button className='rounded-md border-2 border-gray-600 p-2' onClick={() => itemInFav ? removeFromFavorites(product) : addToFavorites(product)}>
-          {
-            itemInFav
-              ? <RemoveFromFavoritesIcon />
-              : <AddFavoritesIcon />
-          }
+    <li className=' bg-gray-900 p-3 rounded-md grid gap-4' key={id}>
+      <div className='flex justify-center items-center flex-col gap-2'>
+        <img className='rounded-md w-full aspect-video block object-cover bg-white' src={thumbnail} alt={id} />
+        <h1 className='text-2xl text-center text-orange-600'>{title}</h1>
+        <h3 className='text-2xl text-slate-200'>${price}</h3>
+      </div>
+      <section className='flex gap-3'>
+        <div className='flex flex-col gap-2 w-full'>
+          <ButtonAddOrRemoveCart
+            product={product}
+            itemInCart={itemInCart}
+            addToCart={addToCart}
+            removeFromCart={removeFromCart}
+          />
+          <ButtonAddOrRemoveFavorites
+            product={product}
+            itemInFav={itemInFav}
+            addToFavorites={addToFavorites}
+            removeFromFavorites={removeFromFavorites}
+          />
+        </div>
+        <button className='rounded-md border-2 bg-zinc-400 border-zinc-400 p-2 flex items-center w-2/5'>
+          More<ExternalLink />
         </button>
       </section>
     </li>
