389c8e6da317919063dcdeb8c80a2737596ccd86
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-31 23:34:30 -0300

    style header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,13 @@ export const Header = () => {
   return (
     <header className='bg-slate-900 w-full'>
       <nav className='max-w-7xl flex justify-between p-4 items-center m-auto'>
-        <NavLink className='text-6xl' to='/'> SHOPPING CART </NavLink>
+        <NavLink className='text-6xl text-orange-500' to='/'> SHOPPING CART </NavLink>
         <section className='flex gap-20'>
-          <NavLink className='flex gap-4 items-center' to='/cart'>
+          <NavLink className='flex gap-4 items-center text-green-400' to='/cart'>
             <span className='text-2xl'>Cart</span>
             <CartIcon />
           </NavLink>
-          <NavLink className='flex gap-4 items-center' to='/favorites'>
+          <NavLink className='flex gap-4 items-center text-red-400' to='/favorites'>
             <span className='text-2xl'>Favorites</span>
             <AddFavoritesIcon />
           </NavLink>
