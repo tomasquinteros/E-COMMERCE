e05118254f85ae91e8f14eb6050249c8f51ab146
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-04-01 00:42:01 -0300

    style and import clearFavorites

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -4,10 +4,11 @@ import { RemoveFromFavoritesIcon } from './Icons'
 function ItemFavorites ({ favorites, removeFromFavorites }) {
   return (
     favorites.map(product => (
-      <li key={product.id}>
-        <h1>{product.title}</h1>
-        <span>${product.price}</span>
-        <button onClick={() => removeFromFavorites(product)}>
+      <li key={product.id} className='bg-slate-700 p-4 flex gap-4 flex-col items-center'>
+        <h1 className='text-3xl text-orange-600'>{product.title}</h1>
+        <img className='aspect-video object-cover w-64 bg-white' src={product.thumbnail} alt={product.title} />
+        <p className='text-xl text-white font-bold'>${product.price}</p>
+        <button className='bg-red-600 p-2' onClick={() => removeFromFavorites(product)}>
           <RemoveFromFavoritesIcon />
         </button>
       </li>
@@ -17,12 +18,15 @@ function ItemFavorites ({ favorites, removeFromFavorites }) {
 }
 
 export const Favorites = () => {
-  const { favorites, removeFromFavorites } = useFavorites()
+  const { favorites, removeFromFavorites, clearFavorites } = useFavorites()
   return (
-    <div>
-      <ul>
+    <div className='w-full max-w-6xl'>
+      <ul className='grid grid-cols-3 gap-3'>
         <ItemFavorites favorites={favorites} removeFromFavorites={removeFromFavorites} />
       </ul>
+      <button className='bg-red-600 p-4 gap-4 rounded-lg flex text-xl font-bold' onClick={() => clearFavorites()}>
+        Clear Favorites <RemoveFromFavoritesIcon />
+      </button>
     </div>
   )
 }
