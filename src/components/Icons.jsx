7072e91e6841695317a2be0040924cd4bb4ff85a
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-03-31 23:34:41 -0300

    add ExternalLinkIcon

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,3 +1,14 @@
+export function ExternalLink () {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' className='icon icon-tabler icon-tabler-external-link' width='24' height='24' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'>
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5' />
+      <path d='M10 14l10 -10' />
+      <path d='M15 4l5 0l0 5' />
+    </svg>
+  )
+}
+
 export function AddFavoritesIcon () {
   return (
     <svg xmlns='http://www.w3.org/2000/svg' className='icon icon-tabler icon-tabler-heart-filled' width='24' height='24' viewBox='0 0 24 24' strokeWidth='2' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'>
