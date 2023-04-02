9f7861579e250f9cf90164b4a9b989973d8da26f
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-04-01 22:26:57 -0300

    add function ScrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,32 @@ import { Header } from './components/Header'
 import { Cart } from './components/Cart'
 import { Favorites } from './components/Favorites'
 import { Footer } from './components/Footer'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { Product } from './components/Product'
+import { useEffect } from 'react'
 
 function App () {
+  const ScrollToTop = () => {
+    const { pathname } = useLocation()
+    useEffect(() => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      })
+    }, [pathname])
+  }
   return (
     <FavoritesProvider>
-      <div className='flex flex-col gap-20 items-center justify-center m-auto'>
+      <div className='flex flex-col gap-10 items-center justify-center m-auto'>
         <BrowserRouter>
+          <ScrollToTop />
           <Header />
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/cart' element={<Cart />} />
             <Route path='/favorites' element={<Favorites />} />
+            <Route path='/product/:title' element={<Product />} />
           </Routes>
           <Footer />
         </BrowserRouter>
