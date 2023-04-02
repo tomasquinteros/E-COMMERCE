db078ca97f8fd9ef1d01676d28894834d74b5af9
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-04-01 22:26:38 -0300

    import link react router and add style

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,12 +2,13 @@ import { ExternalLink } from './Icons'
 import { ButtonAddOrRemoveCart, ButtonAddOrRemoveFavorites } from './Buttons'
 import { useCart } from '../hooks/useCart'
 import { useFavorites } from '../hooks/useFavorites'
+import { Link } from 'react-router-dom'
 export const ProductList = ({ product, itemInCart, itemInFav }) => {
   const { title, id, price, thumbnail } = product
   const { addToCart, removeFromCart } = useCart()
   const { removeFromFavorites, addToFavorites } = useFavorites()
   return (
-    <li className=' bg-gray-900 p-3 rounded-md grid gap-4' key={id}>
+    <li className=' bg-gray-900 p-3 rounded-md grid gap-4 shadow-md shadow-slate-700' key={id}>
       <div className='flex justify-center items-center flex-col gap-2'>
         <img className='rounded-md w-full aspect-video block object-cover bg-white' src={thumbnail} alt={id} />
         <h1 className='text-2xl text-center text-orange-600'>{title}</h1>
@@ -28,9 +29,8 @@ export const ProductList = ({ product, itemInCart, itemInFav }) => {
             removeFromFavorites={removeFromFavorites}
           />
         </div>
-        <button className='rounded-md border-2 bg-zinc-400 border-zinc-400 p-2 flex items-center w-2/5'>
-          More<ExternalLink />
-        </button>
+        <Link className='rounded-md border-2 bg-zinc-400 border-zinc-400 p-2 flex items-center w-2/5 hover:bg-zinc-600 transition-all hover:scale-95' to={`/product/${title}`}>More <ExternalLink />
+        </Link>
       </section>
     </li>
   )
