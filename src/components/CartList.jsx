32c04aa5cbcbfe1f37a81631e222fad3cc88df91
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-04-02 23:17:55 -0300

    fix error quantity cart

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -1,5 +1,6 @@
 import { RemoveFromCartIcon } from './Icons'
 export function CartList ({ products, addToCart, removeFromCart, decrementCart }) {
+  const Qty = 1
   return (
     products.map(product => (
       <li key={product.id} className='flex items-center gap-8 px-8 py-2 border-y-2 rounded-2xl border-slate-500 bg-slate-500'>
@@ -12,7 +13,7 @@ export function CartList ({ products, addToCart, removeFromCart, decrementCart }
         </section>
         <section className='flex gap-6 items-center ml-auto'>
           <div className='text-3xl flex  gap-2 items-center'>
-            <button onClick={() => addToCart(product)} className='bg-green-600 p-2 text-white font-extrabold rounded-xl px-5 py-1'>
+            <button onClick={() => addToCart(product, Qty)} className='bg-green-600 p-2 text-white font-extrabold rounded-xl px-5 py-1'>
               +
             </button>
             <span className='text-2xl bg-gray-300 px-4 rounded-lg'>{product.quantity}</span>
